2b8eade840f0a25ed007463ff5c918dc31f3479e
Matei Biciușcă <white.hollow.ro@gmail.com>
2023-02-09 07:19:55 +0200

    un controller + ceva la program.cs RIP frontend =((((

--- Controllers/RezervareController.cs
+++ Controllers/RezervareController.cs
@@ -6,3 +6,38 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using NewRepo.Data;
 using NewRepo.Helpers.Attributes;
+using NewRepo.Models;
+using NewRepo.Services.ReservService;
+
+namespace NewRepo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RezervareController: ControllerBase
+    {
+        private readonly IReservService _reservService;
+
+        public RezervareController(IReservService reservService)
+        {
+            _reservService = reservService;
+        }
+
+        [HttpPost("Adauga-rezervare")]
+        public async Task<IActionResult> Create(RezervareDTO rez)
+        {
+            return await _reservService.CreateRezAsync(rez);
+        }
+
+        [HttpGet("{UserId}")]
+        public async Task<IActionResult> GetReserv(Guid UserId)
+        {
+            var rez = await _reservService.GetOrderByUserId(UserId);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateReserv(RezervareDTO rez, bool status)
+        {
+            return await _reservService.UpdateRezStatus(rez, status);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,19 +1,41 @@
+using Microsoft.EntityFrameworkCore;
+using NewRepo.Data;
+using NewRepo.Helpers;
+using NewRepo.Helpers.Extensions;
+using NewRepo.Helpers.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<DataBaseContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
+builder.Services.AddRepositories();
+builder.Services.AddServices();
+builder.Services.AddSeeders();
+builder.Services.AddUtils();
+
+builder.Services.Configure<AppSettings>(builder.Configuration.GetSection("AppSettings"));
+
+//AutoMapper
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+//Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
+
 app.UseHttpsRedirection();
+
+app.UseAuthorization();
+app.UseMiddleware<JwtMiddleware>();
 app.UseStaticFiles();
 app.UseRouting();
 
